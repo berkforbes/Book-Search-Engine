d598e96a4979ffa4328759a25663f008f0e33e9f
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-04-03 23:35:59 -0600

    debugging

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,67 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import {useQuery, useMutation} from '@apollo/react-hooks';
-import {GET_ME} from '../utils/queries';
-import {REMOVE_BOOK} from '../utils/mutations';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { removeBookId } from '../utils/localStorage';
-
+import React, { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const {loading, data: userData} = useQuery(GET_ME);
-  const [removeBook, {error}] = useMutation(REMOVE_BOOK);
+  const { loading, data } = useQuery(GET_ME);
 
-  if(!userData){
-      return null
+  const [userData, setData] = useState(loading ? null : data.me);
+
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+
+  if (!userData) {
+    return null;
   }
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     try {
       const data = await removeBook({
-        variables: {bookId},
+        variables: { bookId },
+      });
+
+      // update state of books:
+      setData(() => {
+        return {
+          ...userData,
+          savedBooks: data.data.removeBook.savedBooks,
+        };
       });
     } catch (err) {
       console.error(err);
     }
     removeBookId(bookId);
-    window.location.reload(false);
   };
-
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {!loading && userData.me.savedBooks.length
-            ? `Viewing ${userData.me.savedBooks.length} saved ${userData.me.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'
-          }
+          {!loading && userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <CardColumns>
-          {!loading && userData.me.savedBooks.map((book) => {
+          {!loading &&
+            userData.savedBooks.map((book) => {
               return (
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card key={book.bookId} border="dark">
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                    />
+                  ) : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
                       Delete this Book!
                     </Button>
                   </Card.Body>
                 </Card>
               );
-            })
-          }
+            })}
         </CardColumns>
       </Container>
     </>
   );
 };
+
 export default SavedBooks;
